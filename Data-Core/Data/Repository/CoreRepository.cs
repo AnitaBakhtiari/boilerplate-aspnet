e82cd7f8e658e2379146ccca4ed7763a42cdf5d8
AnitaBakhtiari <anitabakhtiari@gmail.com>
2022-06-26 07:29:28 +0430

    edit core repository

--- Data-Core/Data/Repository/CoreRepository.cs
+++ Data-Core/Data/Repository/CoreRepository.cs
@@ -1,5 +1,71 @@
-﻿namespace DataCore.Data.Repository;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class CoreRepository<T> : ICoreRepository<T> where T : class
+namespace DataCore.Data.Repository;
+
+public class CoreRepository<TEntity,TContext> : ICoreRepository<TEntity> where TEntity : class where TContext : DbContext
 {
+    protected readonly TContext _context;
+
+    public CoreRepository(TContext context)
+    {
+        _context = context;
+    }
+    public TEntity Add(TEntity entity)
+    {
+        _context.Set<TEntity>().Add(entity);
+        return entity;
+    }
+
+    public async Task<TEntity> GetAsync(long id)
+    {
+        return await _context.Set<TEntity>().FindAsync(id);
+    }
+
+    public async Task<TEntity> DeleteAsync(long id)
+    {
+        var entity = await GetAsync(id);
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        _context.Set<TEntity>().Remove(entity);
+        return entity;
+    }
+    
+    public IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entities)
+    {
+        _context.Set<TEntity>().AddRange(entities);
+        return entities;
+    }
+    public TEntity Delete(TEntity entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        _context.Set<TEntity>().Remove(entity);
+        return entity;
+    }
+    public TEntity Update(TEntity entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        return entity;
+    }
+    public IEnumerable<TEntity> UpdateRange(IEnumerable<TEntity> entities)
+    {
+        foreach (var entity in entities)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        return entities;
+    }
+    public async Task<IEnumerable<TEntity>> GetListAsync()
+    {
+        return await _context.Set<TEntity>().ToListAsync();
+    }
+    public Task<long> CountAsync => _context.Set<TEntity>().LongCountAsync();
+    public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
 }
--- Data-Core/Data/Repository/ICoreRepository.cs
+++ Data-Core/Data/Repository/ICoreRepository.cs
@@ -1,5 +1,16 @@
 ﻿namespace DataCore.Data.Repository;
 
-public interface ICoreRepository<T> where T : class /*: JpaRepository<TEntity, string> where TEntity : ICoreEntity*/
+public interface ICoreRepository<TEntity> where TEntity : class /*: JpaRepository<TEntity, string> where TEntity : ICoreEntity*/
 {
+    IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entities);
+    
+    TEntity Add(TEntity entity);
+    Task<TEntity> GetAsync(long id);
+    Task<TEntity> DeleteAsync(long id);
+    TEntity Update(TEntity entity);
+    IEnumerable<TEntity> UpdateRange(IEnumerable<TEntity> entities);
+    TEntity Delete(TEntity entity);
+    Task<IEnumerable<TEntity>> GetListAsync();
+    Task<long> CountAsync { get; }
+    Task SaveChangesAsync();
 }
