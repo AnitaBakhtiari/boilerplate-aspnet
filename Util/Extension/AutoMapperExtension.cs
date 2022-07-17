98578cd63096d400d93ac311c58d42b016698239
AnitaBakhtiari <anitabakhtiari@gmail.com>
2022-07-17 08:05:02 +0430

    add auto mapper extension

---
+++ Util/Extension/AutoMapperExtension.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Util.Extension;
+
+public static class AutoMapperExtension
+{
+    public static IServiceCollection AddAutoMapperService(this IServiceCollection services, string myAssembly)
+    {
+        var mappingConfig = new MapperConfiguration(mc => { mc.AddMaps(myAssembly); });
+
+        var mapper = mappingConfig.CreateMapper();
+        services.AddSingleton(mapper);
+
+        return services;
+    }
+}
